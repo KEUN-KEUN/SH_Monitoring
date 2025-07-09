784fafd8a99d4b986fb539a6ec3af255a68082d5
KEUN-KEUN <montingy@gmail.com>
2025-07-09 18:39:05 +0900

    환경 설정
    app host, app port

--- app/main.py
+++ app/main.py
@@ -3,11 +3,38 @@ import asyncio
 import warnings
 import aiomysql
 
+from dotenv import load_dotenv
 from fastapi import FastAPI
+
 from config.cors_config import CorsConfig # 설정 파일 보안 등 
 from config.initializer import lifespan   # app의 생명 주기 관리
 
+# 초기 설정
+warnings.filterwarnings("ignore", category=aiomysql.Warning)
+load_dotenv()
 
+# FastAPI 앱 초기화
 app = FastAPI(lifespan=lifespan)
 
-print(123)
+# CORS 설정
+CorsConfig.middlewareConfig(app)
+
+# 웹소켓 연결 상태 저장소
+app.state.connections = set()
+
+
+# 앱 실행
+if __name__ == "__main__":
+    import uvicorn
+
+    print(12)
+
+    host = os.getenv("APP_HOST")
+    port = int(os.getenv("APP_PORT"))
+    
+    print(123)
+    
+    print(f"Starting FastAPI app on {host}:{port}")
+
+    uvicorn.run(app, host=host, port=port)
+
