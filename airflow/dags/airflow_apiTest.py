7e1ea06e0f90f7c18311e82bcbba01e988cbb914
KEUN-KEUN <montingy@gmail.com>
2025-07-22 11:33:20 +0900

    airflow 연동
    ubuntu - apiTest DAG 생성

---
+++ airflow/dags/airflow_apiTest.py
@@ -0,0 +1,30 @@
+import logging
+from datetime import datetime
+
+from airflow import DAG
+
+from airflow.operators.python import PythonOperator
+import requests
+from datetime import timedelta
+
+
+def call_api():
+    response = requests.get("http://172.20.100.95:33333/apitest")
+    logging.info(f"API Response: {response.status_code} - {response.text}")
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2025, 7, 21),
+}
+
+dag = DAG(
+    'api_test_dag',
+    default_args=default_args,
+    schedule_interval=timedelta(seconds=30),  # Every 30 seconds
+)
+
+api_task = PythonOperator(
+    task_id='call_api_task',
+    python_callable=call_api,
+    dag=dag,
+)
